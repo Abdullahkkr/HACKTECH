733342b114f00420b23cf4311d67287118bfa85b
shahfarazz <shahfarazulhaq@gmail.com>
2021-11-27 23:10:06 +0500

    hello

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
     host : 'localhost',
     user : 'root',
     password : '',
-    database : 'Hacktech',
+    // database : 'Hacktech',
     multipleStatements :true
 });
 
@@ -31,7 +31,7 @@ app.use(cors());
 const init_db = `
 CREATE TABLE Admin(Name VARCHAR(50), Admin_ID int(5), Password VARCHAR(64), PRIMARY KEY (Admin_ID));
 CREATE TABLE Orders(Unit_ID bigint(200), Order_ID bigint(200), Order_Confirmation bool, Order_Date date, Delivery_Date date, Courier_Service_Name VARCHAR(50), Customer_ID int(8), Order_Status VARCHAR(20), PRIMARY KEY (Order_ID));
-CREATE TABLE Inventory(Unit_ID bigint(200), Brand VARCHAR(50), Features VARCHAR(5000), Product_Name VARCHAR(100), Colour VARCHAR(20), Description VARCHAR(5000), Images VARCHAR(10), Cost_Price int(20), Selling_Price int(20), Admin_ID int(5), Category VARCHAR(10), PRIMARY KEY (Unit_ID), FOREIGN KEY (Admin_ID) REFERENCES Admin(Admin_ID));
+CREATE TABLE Inventory(Unit_ID bigint(200), Brand VARCHAR(50), Features VARCHAR(5000), Product_Name VARCHAR(100), Colour VARCHAR(20), Description VARCHAR(5000), Images VARCHAR(10), Cost_Price int(20), Selling_Price int(20), Admin_ID int(5), Category VARCHAR(5000), PRIMARY KEY (Unit_ID), FOREIGN KEY (Admin_ID) REFERENCES Admin(Admin_ID));
 CREATE TABLE Customers(Customer_ID int(8), Customer_Name VARCHAR(50), Address VARCHAR(1000), Past_Orders int(200), Email VARCHAR(50), Contact_Number bigint(200), CNIC_Number bigint(200), PRIMARY KEY (Customer_ID));
 CREATE TABLE Accounts(Customer_ID int(8), Password VARCHAR(64), PRIMARY KEY (Customer_ID), FOREIGN KEY (Customer_ID) REFERENCES Customers(Customer_ID));
 CREATE TABLE Printer(Unit_ID bigint(200), Wireless VARCHAR(3), Type VARCHAR(50), Portable VARCHAR(3), PRIMARY KEY (Unit_ID), FOREIGN KEY (Unit_ID) REFERENCES Inventory(Unit_ID));
@@ -773,10 +773,10 @@ app.post('/inventory/edit', (req, res) => {
 
     if (req.body.Category === "Mobile_Phone")
     {
-        let post_new = {"RAM":req.body.ram, "Memory":req.body.Memory}
-        const sql_new = `Update Mobile_Phone SET ? Where Unit_ID=${req.body.Unit_ID}`;
+        // let post_new = {"RAM":req.body.ram, "Memory":req.body.Memory}
+        const sql_new = `Update Mobile_Phone SET RAM="${req.body.ram}", Memory="${req.body.Memory}" Where Unit_ID=${req.body.Unit_ID}`;
 
-        let body = db.query(sql_new, post_new, (err, result) => {
+        let body = db.query(sql_new, (err, result) => {
             if (err) {
                 return res.send({
                     'isSuccessful':false,
@@ -787,8 +787,8 @@ app.post('/inventory/edit', (req, res) => {
                 });
             }
             else{
-                const sql = `UPDATE Inventory SET Brand=${req.body.Brand}, Features=${req.body.Features}, Product_Name=${req.body.Product_Name}, Colour=${req.body.Colour}, Description=${req.body.Description}, Images=${req.body.Images}, Cost_Price=${req.body.Cost_Price}, Selling_Price=${req.body.Selling_Price}, Admin_ID=${req.body.Admin_ID}, Category=${req.body.Category}, where Unit_ID=${req.body.Unit_ID}`;
-                let body = db.query(sql, post, (err, result) => {
+                const sql = `UPDATE Inventory SET Brand="${req.body.Brand}", Features="${req.body.Features}", Product_Name="${req.body.Product_Name}", Colour="${req.body.Colour}", Description="${req.body.Description}", Images="${req.body.Images}", Cost_Price=${req.body.Cost_Price}, Selling_Price=${req.body.Selling_Price}, Admin_ID=${req.body.Admin_ID}, Category="${req.body.Category}" where Unit_ID=${req.body.Unit_ID}`;
+                let body = db.query(sql, (err, result) => {
                     if (err) {
                         return res.send({
                             'isSuccessful':false,
