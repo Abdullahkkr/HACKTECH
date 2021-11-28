8cd178c2ddba772265ef2bb76b6c6d8a0efd8f82
Fawaz25 <23100025@lums.edu.pk>
2021-11-28 19:07:00 +0500

    final

--- client/components/Headers/UserStats.js
+++ client/components/Headers/UserStats.js
@@ -1,13 +1,35 @@
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+// import CardStats from "../Cards/Camera"
 // components
 
 import { allInventory } from "pages/auth/Services-API/api";
 
 export default function HeaderStats() {
+
+    const [products, setProducts] = useState()
+
+    useEffect(() => {
+        allInventory().then((response) =>{
+            setProducts(response.data.result)
+            console.log(response.data);
+        })
+    }, [])
+
 return (
-    allInventory().then((response) =>{
-        console.log(response.data);
-    })
+    <div>
+        {products ? products.map((item, i) => {
+            return (
+                <div key={i} >
+                    <button>
+                    <p>{item.Category}</p>
+                    <p>{item.Brand}</p>
+                    <br/>
+                    <br/>
+                    </button>
+                </div>
+            );
+        }) : null }
+        {/* <CardStats /> */}
+    </div>
 )
 }
