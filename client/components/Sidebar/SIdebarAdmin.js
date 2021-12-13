1d099b17e35b530ac3fda1d7487e104687e8e1f5
Abdullahkkr <23100312@lums.edu.pk>
2021-12-13 22:16:33 +0500

    fixed sidebaradmin import

--- client/components/Sidebar/SIdebarAdmin.js
+++ client/components/Sidebar/SIdebarAdmin.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import NotificationDropdown from "components/Dropdowns/NotificationDropdown.js";
 import UserDropdown from "components/Dropdowns/UserDropdown.js";
 
-export default function Sidebar2() {
+export default function SidebarAdmin() {
   const [collapseShow, setCollapseShow] = React.useState("hidden");
   const router = useRouter();
   return (
--- client/layouts/EditItem.js
+++ client/layouts/EditItem.js
@@ -3,7 +3,7 @@ import React from "react";
 // components
 
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
-import Sidebar from "components/Sidebar/SidebarAdmin.js";
+import SidebarAdmin from "components/Sidebar/SidebarAdmin.js";
 import HeaderStats from "components/Headers/EditHeaderStats.js";
 import FooterAdmin from "components/Footers/FooterAdmin.js";
 
@@ -13,7 +13,7 @@ return (
 <div
     className="absolute top-0 w-full h-full bg-blueGray-800 bg-full"
         >
-    <Sidebar />
+    <SidebarAdmin />
     <div className="relative md:ml-64 bg-blueGray-100">
     <AdminNavbar />
     <HeaderStats />
