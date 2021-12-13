e4a2fba3184d68e361b50cbca96bb93eb62187da
Abdullahkkr <23100312@lums.edu.pk>
2021-12-13 22:57:14 +0500

    fixed link typo

--- client/components/CardsUserDash/Console.js
+++ client/components/CardsUserDash/Console.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import Link from "next/Link";
+import Link from "next/link";
 import { useAtom } from "jotai";
 import { getCategoryAtom } from "pages/userState";
 
--- client/components/CardsUserSide/Console.js
+++ client/components/CardsUserSide/Console.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import Link from "next/Link";
+import Link from "next/link";
 
 
 export default function CardStats({
